e5ec058d411ebce9947e43f8bf6dc503fc4f22a8
Cindy Peng <cindypeng@Cindys-MacBook-Pro.local>
2020-11-27 09:47:58 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-//import logo from './logo.svg';
-//import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./component/Home";
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-//import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import reducers from "./reducers/reducer";
